467f100008c1d130c5a99d1d24cbb17a1d3d5695
trickypr <trickypr@users.noreply.github.com>
2019-06-30 16:40:14 +1000

    remove unnecessary code

--- Control_over_serial/commands.ino
+++ Control_over_serial/commands.ino
@@ -1,11 +1,23 @@
+#define version "1.0.0"
+#define updateURL "https://raw.githubusercontent.com/trickypr/Control-over-serial-arduino/master/versions.json"
+#define helpURL "https://github.com/trickypr/Control-over-serial-arduino/wiki"
+
 void callCommand() {
   // Debug and constants
   if (command == "echo") {
     Serial.println(args);
   } else if (command == "help") {
-    help();
-  } else if (command == "verion" || command == "v") {
-    Serial.println(version);
+    const String helpString = "Help is available at: ";
+    helpString += helpURL;
+    
+    Serial.println(helpString);
+  } else if (command == "version" || command == "v") {
+    const String versionString = version;
+    versionString += "\nOriginally by trickypr and the community under GNU AGPLv3";
+    versionString += "\nUpdate URL: ";
+    versionString += updateURL;
+    
+    Serial.println(versionString);
   } else if (command == "reset") {
     setup();
   }
@@ -14,14 +26,14 @@ void callCommand() {
   else if (command == "pinMode") {
     pinMode(gpio, value);
   } else if (command == "digitalRead") {
-    Serial.println(value +' ' + digitalRead(gpio));
+    Serial.println(digitalRead(gpio));
   } else if (command == "digitalWrite") {
     digitalWrite(gpio, value);
   }
 
   // Analog commands
   else if (command == "analogRead") {
-    Serial.println(value + ' ' + analogRead(gpio));
+    Serial.println(analogRead(gpio));
   } else if (command == "analogWrite") {
     analogWrite(gpio, value);
   }
@@ -34,13 +46,13 @@ void callCommand() {
   } else if (command == "delayMicroseconds") {
     delayMicroseconds(gpio);
   } else if (command == "micros") {
-    Serial.println(gpio + ' ' + micros());
+    Serial.println(micros());
   } else if (command == "millis") {
-    Serial.println(gpio + ' ' + millis());
+    Serial.println(millis());
   }
 
   // Unknown commands
   else {
-    Serial.println("Unrecognized command,\ntype \"help\" for command list");
+    Serial.println("Unrecognized command, type \"help\" for command list");
   }
 }
